8ecf6cd24be414630cf3c25c498fed293262a4af
Nathan Anderson <nathan.anderson.1024@gmail.com>
2022-09-20 18:24:43 -0700

    completed the take home challenge

---
+++ src/Components/utils/utils.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export default function utils() {
+  const getMoviesByName = async (inputString) => {
+    const apiKey = '';
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&t=${inputString}`);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  }
+  const getMovieDetailsById = async (omdbID) => {
+    const apiKey = '';
+    const response = await fetch(`http://www.omdbapi.com/?apikey=${apiKey}&i=${omdbID}`);
+    const jsonResponse = await response.json();
+    console.log(jsonResponse);
+  }
+  return (
+    <div></div>
+  )
+}
